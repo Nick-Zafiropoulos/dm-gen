52c7d2b743b2f60e7a16ed0cd7b01dfa0172bc51
Nick-Zafiropoulos <102330367+Nick-Zafiropoulos@users.noreply.github.com>
2023-02-15 17:16:26 -0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -36,4 +36,4 @@ app.use('/api/npcs', require('./routes/npcRoutes'));
 
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, '0.0.0.0');
